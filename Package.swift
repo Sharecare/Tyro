90ca32f0c8b733e21353b271d23145ed27b49afb
dimitrios.georgakopoulos <dgeorgakopoulos@sharecare.com>
2020-12-19 21:39:16 +0100

    Attempt for swift package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Tyro",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Tyro",
+            targets: ["Tyro-iOS"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/typelift/Swiftz.git", from: "0.0.0"),
+        .package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.8.1")
+    ],
+    targets: [
+        .target(
+            name: "Tyro",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "Tyro-iOS",
+            dependencies: ["Tyro", "Swiftz", "SwiftCheck"])
+    ]
+)
